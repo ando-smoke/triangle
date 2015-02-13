114d1c77deb56f1e7d1af3a035fefc19f269f099
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-13 10:14:36 -0800

    Add initial jQuery content for interaction with main html page.

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,17 @@ var triangleType = function(side1, side2, side3) {
 
   return triType;
 };
+
+$(document).ready(function() {
+  $("form#side_lengths").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var triangleTypeResult = triangleType(side1, side2, side3);
+
+    $(".triangle-type").text(triangleTypeResult);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
